e12a98f29d73f858f9af37644ab4b096bd99a0a8
tkuwill <billwang1023@gmail.com>
2022-08-15 20:34:27 +0800

    Update reminder.sh

--- shellscripts/reminder.sh
+++ shellscripts/reminder.sh
@@ -6,7 +6,7 @@ read mins
 echo "Input reminder messages:"
 read msg
 echo "Now time is "
-date | cut -c 33-48
+date "+%T"
 echo "Set a timer for ${mins} minute(s)."
 sleep ${mins}m && notify-send -u critical -t 10000 ${msg}
 mpv ~/Music/lovesongs/God\ knows...\ \'\'The\ Melancholy\ of\ Haruhi\ Suzumiya\'\'.mp3 --start=10 --end=20 --keep-open=no --no-resume-playback --no-terminal --no-video
