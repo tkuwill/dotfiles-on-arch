3a70697869fb1ebdb5fc12d226413b2631c598a1
tkuwill <billwang1023@gmail.com>
2022-08-18 00:12:43 +0800

    Add the option of playing the url you copied by mpv

--- .scripts/dmenu/player.sh
+++ .scripts/dmenu/player.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-# if you want to use this script of dmenu, you have to install "playerctl" at first.
+# if you want to use this script of dmenu, you have to install "playerctl", "mpv" and "xclip" at first.
 
 function now_play {
     playerctl metadata --format "{{ title }} 
 {{ artist }} - {{ album }}"
 }
 
-
+function urls {
+    xclip -o
+}
 
 
 function player {
-    options="Cancel\nPlay-pause\nNext\nPrev\nNow_playing"
+    options="Cancel\nPlay-pause\nNext\nPrev\nNow_playing\nOpen with mpv"
     selected=$(echo -e $options | dmenu -i -p "playerctl")
     if [[ $selected = "Play-pause" ]]; then 
         playerctl play-pause    
@@ -20,6 +22,8 @@ function player {
         playerctl previous
     elif [[ $selected = "Now_playing" ]]; then 
         notify-send -i /home/will/Pictures/sysicon/music.png -t 5000 "$(now_play)"
+    elif [[ $selected = "Open with mpv" ]]; then 
+	mpv $(urls)
     elif [[ $selected = "Cancel" ]]; then 
         return
     fi
