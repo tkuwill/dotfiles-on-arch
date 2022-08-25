5dc8bca0147436e0bdc5a22d8a830475317cec5c
tkuwill <billwang1023@gmail.com>
2022-08-25 19:21:23 +0800

    Now use 'copyq read' instead of 'xclip -o'

--- .scripts/dmenu/player.sh
+++ .scripts/dmenu/player.sh
@@ -7,12 +7,12 @@ function now_play {
 }
 
 function urls {
-    xclip -o
+    copyq read
 }
 
 
 function player {
-    options="Cancel\nPlay-pause\nNext\nPrev\nNow_playing\nOpen with mpv"
+    options="Cancel\nPlay-pause\nNext\nPrev\nNow_playing\nOpen_with_mpv"
     selected=$(echo -e $options | dmenu -i -p "playerctl")
     if [[ $selected = "Play-pause" ]]; then 
         playerctl play-pause    
@@ -22,7 +22,7 @@ function player {
         playerctl previous
     elif [[ $selected = "Now_playing" ]]; then 
         notify-send -i /home/will/Pictures/sysicon/music.png -t 5000 "$(now_play)"
-    elif [[ $selected = "Open with mpv" ]]; then 
+    elif [[ $selected = "Open_with_mpv" ]]; then 
 	mpv $(urls)
     elif [[ $selected = "Cancel" ]]; then 
         return
