3513b87a354e07cba3e9ae5a4ed7e1bf93ae22e5
tkuwill <billwang1023@gmail.com>
2022-08-28 00:40:00 +0800

    Now use urxvt to run this instead of st.

--- .scripts/dmenu/calendar.sh
+++ .scripts/dmenu/calendar.sh
@@ -4,11 +4,11 @@ function calendar {
     options="Cancel\nthis-month\nthis-year\nnext-twelve-months"
     selected=$(echo -e $options | dmenu -i -p "Calendar")
     if [[ $selected = "this-month" ]]; then 
-        st -e zsh -c 'cal; zsh'
+        urxvt -e zsh -c 'cal; zsh'
     elif [[ $selected = "this-year" ]]; then 
-        st -e zsh -c 'cal -y; zsh'
+        urxvt -e zsh -c 'cal -y; zsh'
     elif [[ $selected = "next-twelve-months" ]]; then 
-        st -e zsh -c 'cal -Y; zsh'
+        urxvt -e zsh -c 'cal -Y; zsh'
     elif [[ $selected = "Cancel" ]]; then 
         return
     fi
