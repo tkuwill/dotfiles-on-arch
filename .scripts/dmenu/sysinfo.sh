f40d3537132c91fdd8a8aac7c35c4c5dc9b7ae3d
tkuwill <billwang1023@gmail.com>
2022-09-06 13:39:40 +0800

    Add internet info. Update BAT-temp

--- .scripts/dmenu/sysinfo.sh
+++ .scripts/dmenu/sysinfo.sh
@@ -3,7 +3,7 @@
 # music.png is from <a href="https://www.flaticon.com/free-icons/google-play-music" title="google play music icons">Google play music icons created by Mayor Icons - Flaticon</a>
 # battery.png is from <a href="https://www.flaticon.com/free-icons/full-battery" title="full battery icons">Full battery icons created by Pixel perfect - Flaticon</a>.
 # cpu.png is from <a href="https://www.flaticon.com/free-icons/thermometer" title="thermometer icons">Thermometer icons created by Smashicons - Flaticon</a>.
-# fan.png is from <a href="https://www.flaticon.com/free-icons/fan" title="fan icons">Fan icons created by Nikita Golubev - Flaticon</a>.
+# wifi.png is from <a href="https://www.flaticon.com/free-icons/wifi" title="wifi icons">Wifi icons created by Gregor Cresnar - Flaticon</a>.
 # You have to install lm_sensors, acpi, libnotify and a notification package (like dunst) in order to use this script.
 
 function mem {
@@ -23,22 +23,27 @@ function cpu_temp {
     sensors | grep 'Core 1' | cut -c 15-23
 }
 
-function bat_temp {
-    sensors > /tmp/sensors.txt && cat -n /tmp/sensors.txt | grep '12' | cut -c 21-29
+function wifi {
+    nmcli device | grep 'wlan0          wifi' | cut -c 40-55
 }
 
+function bat_temp {
+    acpi --thermal | cut -c 15-19
+}
 
 function sysinfo {
-    options="Cancel\nMemory\nBAT-remaining\ncpu_temp\nBAT_temp"
+    options="Cancel\nMemory\nBAT-remaining\ninternet\ncpu_temp\nBAT_temp"
     selected=$(echo -e $options | dmenu -i -p "System info")
     if [[ $selected = "Memory" ]]; then 
         notify-send -i /home/will/Pictures/sysicon/ram.png -t 8000 "Mem is used : $(mem)  /  $(allmem)." 
     elif [[ $selected = "BAT-remaining" ]]; then 
         notify-send -i /home/will/Pictures/sysicon/battery.png -t 8000 "BAT-time : $(battime) - remaining."  
+    elif [[ $selected = "internet" ]]; then 
+        notify-send -i /home/will/Pictures/sysicon/wifi.png -t 8000 "$(wifi) connected."  
     elif [[ $selected = "cpu_temp" ]]; then 
         notify-send -i /home/will/Pictures/sysicon/cpu.png -t 5000 "CPU temp: $(cpu_temp) ."
     elif [[ $selected = "BAT_temp" ]]; then 
-        notify-send -i /home/will/Pictures/sysicon/battery.png -t 8000 "BAT temp : $(bat_temp) ."  
+        notify-send -i /home/will/Pictures/sysicon/battery.png -t 8000 "BAT temp : $(bat_temp) °C."  
     elif [[ $selected = "Cancel" ]]; then 
         return
     fi
