e69cf36d8cea1f079f91365a7b39600a18199300
tkuwill <billwang1023@gmail.com>
2022-09-25 09:50:30 +0800

    Add a new option to open bilibili on the fly.

--- .scripts/dmenu/browser.sh
+++ .scripts/dmenu/browser.sh
@@ -4,12 +4,14 @@
 
 
 function browser {
-    options="Cancel\nChromium\nEdge"
+    options="Cancel\nChromium\nEdge\nbilibili"
     selected=$(echo -e $options | dmenu -i -p "Which browser do you want to use ?")
     if [[ $selected = "Chromium" ]]; then 
         chromium --force-device-scale-factor=1.3    
     elif [[ $selected = "Edge" ]]; then 
         microsoft-edge-stable --force-device-scale-factor=0.9
+    elif [[ $selected = "bilibili" ]]; then 
+        firefox  --new-window https://www.bilibili.com/
     elif [[ $selected = "Cancel" ]]; then 
         return
     fi
