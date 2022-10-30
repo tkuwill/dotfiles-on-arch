8104b7f938ec294ed83c69ec447468ddc1e45408
tkuwill <billwang1023@gmail.com>
2022-10-30 19:33:54 +0800

    Add cmusvol cmd to adjust vol of cmus in a scratchpad of st.

--- dwm/config.h
+++ dwm/config.h
@@ -67,7 +67,8 @@ static const Layout layouts[] = {
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "32x10", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "45x15", NULL };
+static const char *cmusvolcmd[] = { "st", "-t", scratchpadname, "-g", "32x10", "-e", "zsh", "-c", "/home/will/shellscripts/cmusvol.sh; zsh", NULL };
 /*screenshot*/
 static const char *shotcmd[]  = { "flameshot", "full", NULL };
 static const char *shotzcmd[]  = { "flameshot", "gui", NULL };
@@ -140,6 +141,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY|ShiftMask,             XK_grave,  togglescratch,  {.v = cmusvolcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
