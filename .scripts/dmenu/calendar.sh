584c9bb850c8c80699f5370d46c1cbb38854da23
tkuwill <billwang1023@gmail.com>
2023-01-15 12:01:52 +0800

    Add `-T` for the window title.

--- .scripts/dmenu/calendar.sh
+++ .scripts/dmenu/calendar.sh
@@ -4,11 +4,11 @@ function calendar {
     options="Cancel\nthis-month\nthis-year\nnext-twelve-months"
     selected=$(echo -e $options | dmenu -i -p "Calendar")
     if [[ $selected = "this-month" ]]; then 
-        urxvt -e zsh -c 'cal; zsh'
+        urxvt -T 'Calendar of this month' -e zsh -c 'cal; zsh'
     elif [[ $selected = "this-year" ]]; then 
-        urxvt -e zsh -c 'cal -y; zsh'
+        urxvt -T 'Calendar of this year' -e zsh -c 'cal -y; zsh'
     elif [[ $selected = "next-twelve-months" ]]; then 
-        urxvt -e zsh -c 'cal -Y; zsh'
+        urxvt -T 'Calendar of next 12 months' -e zsh -c 'cal -Y; zsh'
     elif [[ $selected = "Cancel" ]]; then 
         return
     fi
