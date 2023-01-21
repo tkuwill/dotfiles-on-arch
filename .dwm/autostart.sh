14370ad0f30dcd683a0b6d9c587b25ad5a57a55d
tkuwill <billwang1023@gmail.com>
2023-01-21 09:08:47 +0800

    Add two commands to use gitui normally in every startup session.

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # General stuff
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+eval `ssh-agent -s` &
+ssh-add &
 feh --bg-fill /home/will/Pictures/sysicon/wallpaper.png &
 fcitx5 &
 copyq &
