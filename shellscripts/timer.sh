d02b74ee407141477b77330d9058b0e33b86dded
tkuwill <billwang1023@gmail.com>
2023-02-03 15:34:22 +0800

    Change the sound of the timer.sh

--- shellscripts/timer.sh
+++ shellscripts/timer.sh
@@ -9,5 +9,6 @@ echo "Now time is "
 date
 echo "Set a timer for ${mins} minute(s)."
 sleep ${mins}m && notify-send -u critical -t 10000 ${mins} 'min(s) timer has Finished.'
-mpv ~/Music/lovesongs/God\ knows...\ \'\'The\ Melancholy\ of\ Haruhi\ Suzumiya\'\'.mp3 --start=10 --end=20 --keep-open=no --no-resume-playback --no-terminal --no-video
+mpv ~/Pictures/sysicon/sysAudio/alarm.ogg --keep-open=no --no-resume-playback --no-terminal --no-video --volume=70
+
 
