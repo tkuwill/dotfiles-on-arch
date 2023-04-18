010447d4a9ef4a89ff72df33e9198a4541550daf
tkuwill <billwang1023@gmail.com>
2023-04-18 12:46:55 +0800

    Change the calendar (use cal again.)

--- .scripts/dmenu/calendar.sh
+++ .scripts/dmenu/calendar.sh
@@ -4,8 +4,8 @@ function calendar {
     options="Cancel\nthis-month\nthis-year\nnext-twelve-months"
     selected=$(echo -e $options | dmenu -i -p "Calendar")
     if [[ $selected = "this-month" ]]; then 
-        # urxvt -T 'Calendar of this month' -e zsh -c 'cal; zsh'
-	st -t 'Calendar of this month' -e zsh -c 'dialog --calendar 'Calendar'  3 40 $(date "+%d %m %Y") && clear; zsh'
+        urxvt -T 'Calendar of this month' -e zsh -c 'cal; zsh'
+	# st -t 'Calendar of this month' -e zsh -c 'dialog --calendar 'Calendar'  3 40 $(date "+%d %m %Y") && clear; zsh'
     elif [[ $selected = "this-year" ]]; then 
         urxvt -T 'Calendar of this year' -e zsh -c 'cal -y; zsh'
     elif [[ $selected = "next-twelve-months" ]]; then 
