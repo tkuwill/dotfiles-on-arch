7c8f2930b97ee0bafb92a4c3675822a65cc9de8a
tkuwill <billwang1023@gmail.com>
2023-04-18 13:14:17 +0800

    Add wifi signal strength in conky.

--- .config/conky/net.sh
+++ .config/conky/net.sh
@@ -2,7 +2,8 @@
 
     STATUS=$(nmcli device | sed '2 q' | sed '1 d' | awk '{print $3}')
         if [ "$STATUS" = "connected" ]; then
-	    nmcli device | sed '2 q' | sed '1 d' | awk '{print $4,$5,$3}'
+	    iwconfig wlan0 | sed '1 q' | awk '{print $4}' | sed -e 's/ESSID:"// ' | sed -e 's/"//' && iwconfig wlan0 | sed -n "6p" | awk '{ print $2 " Connected"}'
+
         elif [ "$STATUS" = "disconnected" ]; then
             nmcli device | sed '2 q' | sed '1 d' | awk '{print $2,$3}'
         fi
