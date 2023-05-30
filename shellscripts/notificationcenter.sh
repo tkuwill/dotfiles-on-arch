351d10165f4901ab8a12ab5546ef0bf942b6fe20
tkuwill <billwang1023@gmail.com>
2023-05-31 02:37:56 +0800

    Rewrite the script.

--- shellscripts/notificationcenter.sh
+++ shellscripts/notificationcenter.sh
@@ -1,7 +1,5 @@
-#!/bin/zsh
+#!/bin/sh
+# Description: show all notification histories stored by dunst. Only can show <= 20 notifications. Dunst can only store 20 histories by default.
 # chat icon is from https://uxwing.com/speaking-bubbles-line-icon.
-function cen {
-    dunstctl count history   
-}
 
-dunstify -a "center" -i /home/will/Pictures/sysicon/chat.png -t 8000 "Notification histories: $(cen)" && sleep 2s && repeat $(cen + 1) { dunstctl history-pop }
+dunstify -a "center" -i /home/will/Pictures/sysicon/chat.png -t 8000 "Notification histories: $(dunstctl count history)" && sleep 2s &&	for i in {1..20}; do dunstctl history-pop; done
