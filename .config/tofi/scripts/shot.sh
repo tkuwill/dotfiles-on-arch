ada70b0bb87211364b6ebf174d1e836d5bd125f4
tkuwill <billwang1023@gmail.com>
2023-05-31 13:01:31 +0800

    Add swappy(img edit tool) after taking a screenshot.

--- .config/tofi/scripts/shot.sh
+++ .config/tofi/scripts/shot.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
-# Description: a tool for screenshot
-# Dependencies: tofi, grim, slurp
+# Description: a tool for screenshot then edit it.
+# Dependencies: tofi, grim, slurp, swappy
 # Icon is from https://uxwing.com/capture-icon.
 if (ps -a | grep tofi >/dev/null); then
   killall -q tofi
 else
-  case $(printf "%s\n" "Cancel" "Full-screenshot" "Select a region for screenshot" | tofi -c ~/.config/tofi/soy-milk) in
+  case $(printf "%s\n" "Cancel" "Full-screenshot" "Select a region for screenshot" | tofi -c ~/.config/tofi/soy-milk2) in
   "Cancel")
         dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/camera.png -t 3500 "Screenshot Aborted." 
     ;;
   "Full-screenshot")
-	grim && mv ~/Pictures/*_grim.png ~/Pictures/screenshots && dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/camera.png -t 3500 "Image saved." 
+	grim - | swappy -f -
+	dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/camera.png -t 3500 "Saved to ~/Pic./Editedshots/." 
     ;;
   "Select a region for screenshot")
-	grim -g "$(slurp -d)" && mv ~/Pictures/*_grim.png ~/Pictures/screenshots && dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/camera.png -t 3500 "Image saved." 
+	grim -g "$(slurp -d)" - | swappy -f -  
+	dunstify -a "changeVolume" -i /home/will/Pictures/sysicon/camera.png -t 3500 "Saved to ~/Pic./Editedshots/." 
     ;;
   esac
 fi
